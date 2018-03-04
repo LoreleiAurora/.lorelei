79757368765f03b4f33384d09a447ed90e22e16f
Lorelei Aurora <iam@l5i.me>
2018-03-04 21:24:48 +0000

    Remove Perl & Go

--- inc/common.sh
+++ inc/common.sh
@@ -2,9 +2,6 @@
 export PATH=$HOME/bin:$DOTFILES/bin:/usr/local/bin:$PATH
 export MANPATH="/usr/local/man:$MANPATH"
 
-# plenv
-eval "$(plenv init -)"
-
 # rbenv
 eval "$(rbenv init -)"
 
@@ -14,6 +11,3 @@ eval "$(pyenv init -)"
 # nvm
 export NVM_DIR="$HOME/.nvm"
 . "/usr/local/opt/nvm/nvm.sh"
-
-# goenv
-eval "$(goenv init -)"
